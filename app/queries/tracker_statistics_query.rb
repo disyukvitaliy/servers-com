1b6cb139511ac967ff6fbe7dc36e18f381721593
Vitali Disiuk <disyukvitaliy@gmail.com>
2022-12-05 16:18:37 +0400

    TrackerStatisticsQuery returns delay_med now

--- app/queries/tracker_statistics_query.rb
+++ app/queries/tracker_statistics_query.rb
@@ -6,6 +6,7 @@ TrackerStatisticsQuery = Struct.new(:params) do
       Sequel.function(:min, :delay).as(:delay_min),
       Sequel.function(:max, :delay).as(:delay_max),
       Sequel.cast_numeric(Sequel.function(:avg, :delay)).as(:delay_avg),
+      Sequel.cast_numeric(Sequel.function(:percentile_cont, 0.5).within_group(:delay)).as(:delay_med),
       Sequel.function(:count).*.filter(delay: nil).as(:attempts_failed),
       Sequel.function(:count).*.as(:attempts_total)
     ).first
